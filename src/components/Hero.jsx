08335214ec1e0098e6bc4cd19bf7133e92632745
SDM2204 <swapnabro3023@gmail.com>
2024-05-20 23:25:59 +0530

    contact details

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants';
 import profilePic from "../assets/kevinRushProfile.jpg";
+import {motion} from "framer-motion";
+
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+  visible: {
+    x:0,
+    opacity: 1,
+    transition: {duration : 0.5, delay:delay},
+  },
+});
 
 const Hero = () => {
   return (
@@ -8,8 +18,10 @@ const Hero = () => {
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Swapna Dip Mandal</h1> 
-                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</span> 
+                    <motion.h1
+                    variants = {container()}
+                    className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Swapna Dip Mandal</h1> 
+                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span> 
                     <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>  
                 </div>
             </div>
